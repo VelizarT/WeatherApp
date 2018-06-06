9dc6fb37c22b7489dd199a5c70fe27076a0580a4
VelizarT <velizar_tonchev@abv.bg>
2018-06-06 13:13:54 +0300

    clear code

--- get-forecast.js
+++ get-forecast.js
@@ -7,14 +7,11 @@ var getForecast = (address) => {
         if(response.data.status=== 'ZERO_RESULTS' || response.data.status=== 'INVALID_REQUEST') {
             throw new Error('Unable to find address');
         }
-        // console.log(JSON.stringify(response.data.results[0].geometry.location.lat));
         var forecastUrl = `https://api.darksky.net/forecast/665f7498ad8965682001332b863e5b2a/${response.data.results[0].geometry.location.lat},${response.data.results[0].geometry.location.lng}?units=si`;
         console.log(response.data.results[0].formatted_address);
-        // console.log(response.data.results[0].geometry.location.lat, response.data.results[0].geometry.location.lng);
         return axios.get(forecastUrl);
     })
     .then((response) => {
-        // console.log(JSON.stringify(response.data, undefined, 2));
         console.log(`The temperature is ${response.data.currently.temperature}. Feels like ${response.data.currently.apparentTemperature}. ${response.data.currently.summary}`);
     })
     .catch((error) => {
@@ -23,7 +20,6 @@ var getForecast = (address) => {
         } else {
             console.log(error.message);
         }
-        // console.log(error);
     });
 }
 
